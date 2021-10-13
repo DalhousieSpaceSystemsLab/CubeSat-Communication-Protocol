23e7508a4dc16ca66387173b50c3bdd41e78136f
amellalalex <github@alexamellal.com>
2021-10-13 15:45:23 -0300

    fixed routing bug + added FIFO support

--- Cubesat Space Protocol/UART/src/client/src/client.c
+++ Cubesat Space Protocol/UART/src/client/src/client.c
@@ -14,6 +14,11 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <termios.h>
+#include <pthread.h>
 
 // Cubesat space protocol
 #include "csp/csp.h"
@@ -23,11 +28,19 @@
 #define SERVER_ADDR 1
 #define CLIENT_ADDR 2
 #define PORT 10
-#define BUF_SIZE 256
+#define BUF_SIZE 250
 #define CONN_TIMEOUT 1000 // in ms
+#define ROUTE_WORD_STACK 500
+#define ROUTE_OS_PRIORITY 1
+
+#define UART_SPEED B115200
+#define UART_PARITY 0
+
+#define FIFO 0
 
 // Glocal variables
 static int io;
+static int fifo_i, fifo_o;
 
 // Function to be used by the CSP library to route packets to the named pipe (FIFO)
 static int fifo_tx(const csp_route_t *ifroute, csp_packet_t *packet);
@@ -38,7 +51,7 @@ static void *fifo_rx(void *parameters);
 
 // Define I/O device interface
 static csp_iface_t csp_io_dev = {
-    .name = "io",
+    .name = "fifo",
     .nexthop = fifo_tx,
     .mtu = BUF_SIZE,
 };
@@ -68,12 +81,88 @@ int main(int argc, char *argv[])
   }
 
   // Open I/O device
-  int io;
-  if ((io = open(argv[1], O_RDWR)) < 0)
+  if (!FIFO)
   {
-    printf("[!] Failed to open I/O device at %s\n", argv[1]);
-    return -1;
+    // DEBUG
+    printf("[i] Entering FIFO mode...\n");
+
+    if ((io = open(argv[1], O_RDWR | O_NOCTTY | O_SYNC)) < 0)
+    {
+      printf("[!] Failed to open I/O device at %s\n", argv[1]);
+      return -1;
+    }
+
+    struct termios tty;
+    if (tcgetattr(io, &tty) != 0)
+    {
+      printf("error %d from tcgetattr", errno);
+      return -1;
+    }
+
+    cfsetospeed(&tty, UART_SPEED);
+    cfsetispeed(&tty, UART_SPEED);
+
+    tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8; // 8-bit chars
+    // disable IGNBRK for mismatched speed tests; otherwise receive break
+    // as \000 chars
+    tty.c_iflag &= ~IGNBRK; // disable break processing
+    tty.c_lflag = 0;        // no signaling chars, no echo,
+                            // no canonical processing
+    tty.c_oflag = 0;        // no remapping, no delays
+    tty.c_cc[VMIN] = 0;     // read doesn't block
+    tty.c_cc[VTIME] = 5;    // 0.5 seconds read timeout
+
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
+
+    tty.c_cflag |= (CLOCAL | CREAD);   // ignore modem controls,
+                                       // enable reading
+    tty.c_cflag &= ~(PARENB | PARODD); // shut off parity
+    tty.c_cflag |= UART_PARITY;
+    tty.c_cflag &= ~CSTOPB;
+    // tty.c_cflag &= ~CRTSCTS;
+
+    if (tcsetattr(io, TCSANOW, &tty) != 0)
+    {
+      printf("error %d from tcsetattr", errno);
+      return -1;
+    }
+
+    // DEBUG
+    printf("[i] Done.\n\n");
   }
+  else
+  {
+    // DEBUG
+    printf("[i] Entering FIFO mode...\n");
+
+    if ((fifo_i = open("fifo_i", O_RDWR)) < 0)
+    {
+      printf("[!] Failed to open fifo_i\n");
+      return -1;
+    }
+    if ((fifo_o = open("fifo_o", O_RDWR)) < 0)
+    {
+      printf("[!] Failed to open fifo_o\n");
+      return -1;
+    }
+
+    // DEBUG
+    printf("[i] Done.\n\n");
+  }
+
+  // DEBUG
+  printf("[i] Starting to route incoming packets into CSP network...\n");
+
+  // Start routing incoming packets into CSP network
+  pthread_t rx_thread;
+  pthread_create(&rx_thread, NULL, fifo_rx, NULL);
+
+  // DEBUG
+  printf("[i] Done.\n\n");
+
+  // Configure default CSP route
+  csp_route_set(CSP_DEFAULT_ROUTE, &csp_io_dev, CSP_NODE_MAC);
+  csp_route_start_task(ROUTE_WORD_STACK, ROUTE_OS_PRIORITY);
 
   // Main loop
   for (;;)
@@ -83,6 +172,9 @@ int main(int argc, char *argv[])
     printf("[?] Enter a message to send to the server: ");
     fgets(msg, BUF_SIZE, stdin);
 
+    // DEBUG
+    printf("[i] Allocating buffer for packet...\n");
+
     // Allocate buffer for new packet
     csp_packet_t *packet = csp_buffer_get(strlen(msg));
     if (!packet)
@@ -91,26 +183,44 @@ int main(int argc, char *argv[])
       continue;
     }
 
+    // DEBUG
+    printf("[i] Done.\n\n");
+
     // Copy string into packet data
     strcpy((char *)packet->data, msg);
 
     // Set packet length
     packet->length = strlen(msg);
 
+    // DEBUG
+    printf("[i] Establishing connection to server...\n");
+
     // Connect to server with 1000ms timeout
     csp_conn_t *conn = csp_connect(CSP_PRIO_NORM, SERVER_ADDR, PORT, CONN_TIMEOUT, CSP_O_NONE);
+
+    // DEBUG
+    printf("[i] Done.\n\n");
+
     printf("Sending: %s\r\n", msg);
     if (!csp_send(conn, packet, 1000)) // csp_send() failed
     {
       printf("[!] Failed to send packet to server\n");
       return -1;
     }
+    // DEBUG
+    printf("[i] Done.\n\n");
+
+    // Impose 1ms delay between each packet to give CSP time to free buffers
+    sleep(1);
+
+    // DEBUG
+    printf("[i] Closing connection...\n");
 
     // Close connection
     csp_close(conn);
 
-    // Impose 1ms delay between each packet to give CSP time to free buffers
-    // sleep(1);
+    // DEBUG
+    printf("[i] Done.\n\n");
   }
 
   return 0;
@@ -120,9 +230,19 @@ int main(int argc, char *argv[])
 static int fifo_tx(const csp_route_t *ifroute, csp_packet_t *packet)
 {
   // Write packets to output device
-  if (write(io, &packet->length, packet->length + sizeof(uint32_t) + sizeof(uint16_t)) < 0)
+  if (!FIFO)
   {
-    printf("[!] Failed to write data packet to output device. SKIPPING.\n");
+    if (write(io, &packet->length, packet->length + sizeof(uint32_t) + sizeof(uint16_t)) < 0)
+    {
+      printf("[!] Failed to write data packet to output device. SKIPPING.\n");
+    }
+  }
+  else
+  {
+    if (write(fifo_o, &packet->length, packet->length + sizeof(uint32_t) + sizeof(uint16_t)) < 0)
+    {
+      printf("[!] Failed to write to fifo_o. SKIPPING.\n");
+    }
   }
 
   // Free packet buffer
@@ -145,11 +265,23 @@ static void *fifo_rx(void *parameters)
   }
 
   // Wait for incoming packet from input device
-  while (read(io, (void *)&packet->length, BUF_SIZE) > 0)
+  if (!FIFO)
   {
-    // Inject received packet into CSP network
-    csp_qfifo_write(packet, &csp_io_dev, NULL);
-    packet = csp_buffer_get(BUF_SIZE);
+    while (read(io, (void *)&packet->length, BUF_SIZE) >= 0)
+    {
+      // Inject received packet into CSP network
+      csp_qfifo_write(packet, &csp_io_dev, NULL);
+      packet = csp_buffer_get(BUF_SIZE);
+    }
+  }
+  else
+  {
+    while (read(fifo_i, (void *)&packet->length, BUF_SIZE) >= 0)
+    {
+      // Inject received packet into CSP network
+      csp_qfifo_write(packet, &csp_io_dev, NULL);
+      packet = csp_buffer_get(BUF_SIZE);
+    }
   }
 
   // done
--- Cubesat Space Protocol/UART/src/server/src/server.c
+++ Cubesat Space Protocol/UART/src/server/src/server.c
@@ -14,6 +14,11 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <termios.h>
+#include <pthread.h>
 
 // Cubesat space protocol
 #include "csp/csp.h"
@@ -25,10 +30,18 @@
 #define MAX_PEND_CONN 5
 #define CONN_TIMEOUT 1000 // in ms
 #define PACKET_TIMEOUT 10 // in ms
-#define BUF_SIZE 256
+#define BUF_SIZE 250
+#define ROUTE_WORD_STACK 500
+#define ROUTE_OS_PRIORITY 1
+
+#define UART_SPEED B115200
+#define UART_PARITY 0
+
+#define FIFO 0
 
 // Glocal variables
 static int io;
+static int fifo_i, fifo_o;
 
 // Function to be used by the CSP library to route packets to the named pipe (FIFO)
 static int fifo_tx(const csp_route_t *ifroute, csp_packet_t *packet);
@@ -38,8 +51,8 @@ static int fifo_tx(const csp_route_t *ifroute, csp_packet_t *packet);
 static void *fifo_rx(void *parameters);
 
 // Define I/O device interface
-static csp_iface_t csp_io_dev = {
-    .name = "io",
+static csp_iface_t csp_if_fifo = {
+    .name = "fifo",
     .nexthop = fifo_tx,
     .mtu = BUF_SIZE,
 };
@@ -69,26 +82,121 @@ int main(int argc, char *argv[])
   }
 
   // Open I/O device
-  if ((io = open(argv[1], O_RDWR)) < 0)
+  if (!FIFO)
   {
-    printf("[!] Failed to open I/O device at %s\n", argv[1]);
-    return -1;
+    // DEBUG
+    printf("[i] Entering UART mode...\n");
+
+    if ((io = open(argv[1], O_RDWR | O_NOCTTY | O_SYNC)) < 0)
+    {
+      printf("[!] Failed to open I/O device at %s\n", argv[1]);
+      return -1;
+    }
+
+    struct termios tty;
+    if (tcgetattr(io, &tty) != 0)
+    {
+      printf("error %d from tcgetattr", errno);
+      return -1;
+    }
+
+    cfsetospeed(&tty, UART_SPEED);
+    cfsetispeed(&tty, UART_SPEED);
+
+    tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8; // 8-bit chars
+    // disable IGNBRK for mismatched speed tests; otherwise receive break
+    // as \000 chars
+    tty.c_iflag &= ~IGNBRK; // disable break processing
+    tty.c_lflag = 0;        // no signaling chars, no echo,
+                            // no canonical processing
+    tty.c_oflag = 0;        // no remapping, no delays
+    tty.c_cc[VMIN] = 0;     // read doesn't block
+    tty.c_cc[VTIME] = 5;    // 0.5 seconds read timeout
+
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
+
+    tty.c_cflag |= (CLOCAL | CREAD);   // ignore modem controls,
+                                       // enable reading
+    tty.c_cflag &= ~(PARENB | PARODD); // shut off parity
+    tty.c_cflag |= UART_PARITY;
+    tty.c_cflag &= ~CSTOPB;
+    // tty.c_cflag &= ~CRTSCTS;
+
+    if (tcsetattr(io, TCSANOW, &tty) != 0)
+    {
+      printf("error %d from tcsetattr", errno);
+      return -1;
+    }
+
+    // DEBUG
+    printf("[i] Done.\n\n");
+  }
+  else
+  {
+    // DEBUG
+    printf("[i] Entering FIFO mode...\n");
+
+    if ((fifo_i = open("fifo_o", O_RDWR)) < 0)
+    {
+      printf("[!] Failed to open fifo_i\n");
+      return -1;
+    }
+    if ((fifo_o = open("fifo_i", O_RDWR)) < 0)
+    {
+      printf("[!] Failed to open fifo_o\n");
+      return -1;
+    }
+
+    // DEBUG
+    printf("[i] Done.\n\n");
   }
 
+  // DEBUG
+  printf("[i] Starting to route incoming packets into CSP network...\n");
+
+  // Start routing incoming packets into CSP network
+  pthread_t rx_thread;
+  pthread_create(&rx_thread, NULL, fifo_rx, NULL);
+
+  // DEBUG
+  printf("[i] Done.\n\n");
+
+  // DEBUG
+  printf("[i] Setting default CSP route\n");
+
   // Configure default CSP route
+  csp_route_set(CSP_DEFAULT_ROUTE, &csp_if_fifo, CSP_NODE_MAC);
+  csp_route_start_task(ROUTE_WORD_STACK, ROUTE_OS_PRIORITY);
+
+  // DEBUG
+  printf("[i] Done.\n\n");
+
+  // DEBUG
+  printf("[i] Configuring server socket...\n");
 
   // Listen for incoming connections
   csp_socket_t *socket = csp_socket(CSP_SO_NONE);
   csp_bind(socket, PORT);
   csp_listen(socket, MAX_PEND_CONN);
 
+  // DEBUG
+  printf("[i] Done.\n\n");
+
   // Main loop
   for (;;)
   {
+    // DEBUG
+    printf("[i] Listening for incoming connections...\n");
+
     // Proces incoming connection
     csp_conn_t *conn;
     if ((conn = csp_accept(socket, CONN_TIMEOUT)))
     {
+      // DEBUG
+      printf("[i] Done.\n\n");
+      // DEBUG
+      printf("[i] Connection established. Parsing incoming packets...\n");
+
       // Process incoming packets
       csp_packet_t *packet;
       while ((packet = csp_read(conn, PACKET_TIMEOUT)) != NULL)
@@ -98,10 +206,19 @@ int main(int argc, char *argv[])
 
       // Free packet buffer
       csp_buffer_free(packet);
+
+      // DEBUG
+      printf("[i] Done.\n\n");
     }
+
+    // DEBUG
+    printf("[i] Closing connection...\n");
 
     // Close connection
     csp_close(conn);
+
+    // DEBUG
+    printf("[i] Done.\n\n");
   }
 
   return 0;
@@ -111,9 +228,19 @@ int main(int argc, char *argv[])
 static int fifo_tx(const csp_route_t *ifroute, csp_packet_t *packet)
 {
   // Write packets to output device
-  if (write(io, &packet->length, packet->length + sizeof(uint32_t) + sizeof(uint16_t)) < 0)
+  if (!FIFO)
   {
-    printf("[!] Failed to write data packet to output device. SKIPPING.\n");
+    if (write(io, &packet->length, packet->length + sizeof(uint32_t) + sizeof(uint16_t)) < 0)
+    {
+      printf("[!] Failed to write data packet to output device. SKIPPING.\n");
+    }
+  }
+  else
+  {
+    if (write(fifo_o, &packet->length, packet->length + sizeof(uint32_t) + sizeof(uint16_t)) < 0)
+    {
+      printf("[!] Failed to write to fifo_o. SKIPPING.\n");
+    }
   }
 
   // Free packet buffer
@@ -136,11 +263,23 @@ static void *fifo_rx(void *parameters)
   }
 
   // Wait for incoming packet from input device
-  while (read(io, (void *)&packet->length, BUF_SIZE) > 0)
+  if (!FIFO)
   {
-    // Inject received packet into CSP network
-    csp_qfifo_write(packet, &csp_io_dev, NULL);
-    packet = csp_buffer_get(BUF_SIZE);
+    while (read(io, (void *)&packet->length, BUF_SIZE) >= 0)
+    {
+      // Inject received packet into CSP network
+      csp_qfifo_write(packet, &csp_if_fifo, NULL);
+      packet = csp_buffer_get(BUF_SIZE);
+    }
+  }
+  else
+  {
+    while (read(fifo_i, (void *)&packet->length, BUF_SIZE) >= 0)
+    {
+      // Inject received packet into CSP network
+      csp_qfifo_write(packet, &csp_if_fifo, NULL);
+      packet = csp_buffer_get(BUF_SIZE);
+    }
   }
 
   // done
