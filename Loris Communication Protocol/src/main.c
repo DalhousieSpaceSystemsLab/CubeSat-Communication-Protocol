e53bdadf3510f4361f0e34c74436e9b71476c1d7
amellalalex <github@alexamellal.com>
2022-02-07 17:59:41 -0400

    added some code to main

--- Loris Communication Protocol/src/main.c
+++ Loris Communication Protocol/src/main.c
@@ -1,3 +1,16 @@
+// Reed-solomon library
+#include "correct.h"
+
+// Standard C libraries
+#include <stdio.h>
+
 int main() {
+  // Create encoder/decoder
+  correct_reed_solomon * coder = correct_reed_solomon_create(correct_rs_primitive_polynomial_8_4_3_2_0, 1, 1, );
+  if(coder == NULL) {
+    printf("[!] Failed to create encoder/decoder\n");
+    return -1;
+  }
+
   return 0;
 }
