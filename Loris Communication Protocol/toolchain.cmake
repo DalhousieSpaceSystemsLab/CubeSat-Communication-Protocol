2e4abb460f8b343e0d1a4c3c5310aef2fb0d1f6c
amellalalex <github@alexamellal.com>
2022-03-01 18:07:53 -0400

    added toolchain file

---
+++ Loris Communication Protocol/toolchain.cmake
@@ -0,0 +1,13 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+# set(CMAKE_SYSROOT /usr/local/lib/arm-none-linux-gnueabihf/arm-none-linux-gnueabihf)
+
+set(tools /usr/arm-none-linux-gnueabihf)
+set(CMAKE_C_COMPILER arm-none-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER arm-none-linux-gnueabihf-g++)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
